5b4c6129cf73e7f37314a0cf7effc3c078b8a451
Lautern <silin.nikita@gmail.com>
2015-04-27 01:46:05 +0600

    explanation added

--- perfect_number.py
+++ perfect_number.py
@@ -1,3 +1,5 @@
+#This code computes perfect numbers. Perfect number is a number sum of which divisors, divided by 2, 
+#is equal to the number itself. For example 6 is a perfect number because (1+2+3+6)/2 = 6. Next perfect number is 28
 i = 1
 while i < 1000:
     array_of_divisors = []
