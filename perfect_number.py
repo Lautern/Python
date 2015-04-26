454d1e508e772b6fd5d1db2a208190b5ea428320
Lautern <silin.nikita@gmail.com>
2015-04-27 01:58:56 +0600

    some simplifications are made (1+2+3) = 6

--- perfect_number.py
+++ perfect_number.py
@@ -4,10 +4,10 @@ i = 1
 while i < 1000:
     array_of_divisors = []
     sum_of_divisors = []
-    for j in range(1, i+1):
+    for j in range(1, i+1): #can be range(1, i)
         if i % j == 0:
             array_of_divisors.append(j)
     sum_of_divisors = sum(array_of_divisors)
-    if (sum_of_divisors / 2 == i):
-        print("Perfect number:", i)
+    if (sum_of_divisors / 2 == i): #then here it would be: if sum_of_divisors == i
+        print("Perfect number:", i) #in this case it will compute (1+2+3) = 6
     i += 1
